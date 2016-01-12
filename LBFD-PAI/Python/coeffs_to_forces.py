b6f98f34bd7f0476d318e31d3b6ec05884695793
cmheath <christopher.m.heath@nasa.gov>
2016-01-12 11:14:48 -0500

    Small updates to output format.

--- LBFD-PAI/Python/coeffs_to_forces.py
+++ LBFD-PAI/Python/coeffs_to_forces.py
@@ -26,7 +26,7 @@ IDs = ['Aircraft', 'Inlet', 'Nozzle', 'Inlet-IML', 'Nozzle-IML', 'Spinner', 'Plu
 
 L_net = D_net = Fx_net = Fz_net = 0
 
-with open('LBFD.forces.3') as f:
+with open('LBFD.forces') as f:
     for bndry in IDs:
         line = f.readline()
 
@@ -61,11 +61,11 @@ with open('LBFD.forces.3') as f:
             Fx_net += Fx
             Fz_net += Fz
 
-        print ("%s  L = %s, D = %s, Fx = %s, Fz = %s\n" % (bndry.ljust(10), str(L).ljust(10), str(D).ljust(10), str(Fx).ljust(10), str(Fz).ljust(10)))
+        print ("%s  L = %s, D = %s, L/D = %s, Fx = %s, Fz = %s\n" % (bndry.ljust(10), str(L).ljust(10), str(D).ljust(10), str(L/D).ljust(10), str(Fx).ljust(10), str(Fz).ljust(10)))
 
         f.seek(0,0)
 
-print ("Net Lift = %.6f lbf     Net Drag = %.6f lbf\n" % (L_net, D_net))
-#print ("Net Fx = %.6f     Net Fz = %.6f\n" % (Fx_net, Fz_net))
+print ("Net Lift (incl. thrust-induced) = %.6f lbf     Net Drag = %.6f lbf\n" % (L_net, D_net))
+
 print ("Net Thrust (In-line) = %.6f lbf\n" % (0.5*rhozm * symmetry * (xmach*azm)**2 * Fx_net * N_to_lbf))
 print ("Net Thrust (Installed) = %.6f lbf" % (0.5*rhozm * symmetry * (xmach*azm)**2 * (Fx_net*np.cos(alpha) + Fz_net*np.sin(alpha)) * N_to_lbf))
